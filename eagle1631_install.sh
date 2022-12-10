d8ce1ab56e5bd248f3b7dc57d56bb19ece6db2cf
ShaheenHossain <rapidgrps@princegroup-bd.com>
2022-12-10 17:11:35 +0600

    Update eagle1631_install.sh

--- eagle1631_install.sh
+++ eagle1631_install.sh
@@ -1,8 +1,8 @@
-OE_USER="eagle1633"
+OE_USER="eagle1631"
 OE_HOME="/$OE_USER"
 OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
 INSTALL_WKHTMLTOPDF="True"
-OE_PORT="8033"
+OE_PORT="8031"
 OE_VERSION="master"
 IS_ENTERPRISE="False"
 # Installs postgreSQL V14 instead of defaults (e.g V12 for Ubuntu 20/22) - this improves performance
@@ -90,7 +90,7 @@ else
 fi
 
 echo -e "\n---- Create Eagle system user ----"
-sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'EAGLE1633' --group $OE_USER
+sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'EAGLE1631' --group $OE_USER
 #The user should also be added to the sudo'ers group.
 sudo adduser $OE_USER sudo
 
