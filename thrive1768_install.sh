54587e2f60251c5bdf881049cfdc43631fc03c47
ShaheenHossain <rapidgrps@princegroup-bd.com>
2023-11-24 20:57:54 +0600

    Update thrive1768_install.sh

--- thrive1768_install.sh
+++ thrive1768_install.sh
@@ -99,10 +99,10 @@ sudo mkdir /var/log/$OE_USER
 sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 
 #--------------------------------------------------
-# Install Eagle
+# Install Eagle 
 #--------------------------------------------------
 echo -e "\n==== Installing Eagle Server ===="
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/u16052642/thrive_bureau_erp $OE_HOME_EXT/
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/u16052642/thrive_bureau_erp17 $OE_HOME_EXT/
 
 if [ $IS_ENTERPRISE = "True" ]; then
     # Eagle Enterprise install!
